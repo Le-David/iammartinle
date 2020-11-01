10732ccd9d2d72f2bddb37c099fe4d41fcec1d10
Quang Dat Le (David) <lequangdatcz@gmail.com>
2020-11-02 00:46:33 +0100

    set up post_type "articles"

--- theme/functions.php
+++ theme/functions.php
@@ -1,3 +1,23 @@
 <?php
 
+// remove (wautop) tags "p" when using "apply_filters"
+remove_filter( 'the_content', 'wpautop' );
+remove_filter( 'the_excerpt', 'wpautop' );
+
 initTheme();
+
+function getArticles(){
+
+    $totalPages = get_option('posts_per_page'); // get number of posts per page from settings in admin (settings->reading->Blog pages show at most)
+
+	$query = new WP_Query([
+		'post_type' =>'articles',
+		'post_status' =>'publish',
+		'order' => 'DESC',
+		'orderby' => 'date',
+		'posts_per_page' => $totalPages,
+	]);
+
+    return $query;
+	
+}
