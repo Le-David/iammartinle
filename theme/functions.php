0a339a6d46ab56d7399d79cbe45fc9cd5a9c61d1
Quang Dat Le (David) <lequangdatcz@gmail.com>
2020-11-02 23:32:23 +0100

    adjust navigation/menu

--- theme/functions.php
+++ theme/functions.php
@@ -6,6 +6,16 @@ remove_filter( 'the_excerpt', 'wpautop' );
 
 initTheme();
 
+// add class "active" to active link in navigation
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class ($classes, $item) {
+    if (in_array('current-page-ancestor', $classes) || in_array('current-menu-item', $classes) || in_array('current_page_parent', $classes) ){
+        $classes[] = 'view-active ';
+    }
+    return $classes;
+}
+
 function getArticles(){
 
     $totalPages = get_option('posts_per_page'); // get number of posts per page from settings in admin (settings->reading->Blog pages show at most)
